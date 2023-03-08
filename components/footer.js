854ce2bb395ddf7247334262dd4e29873bc23c00
Codehash001 <codehashblockchain@gmail.com>
2023-03-08 21:02:48 +0530

    update

--- components/footer.js
+++ components/footer.js
@@ -1,18 +1,18 @@
 export default function Footer(){
     return(
-        <div className=" bg-black w-full h-auto md:px-16 px-2 mt-4">
+        <div className=" bg-black w-full h-auto md:px-16 px-2 py-4">
 
-<footer class="py-6 md:px-6 text-white font-Kanit">
+<footer class="text-white font-Kanit">
 	<div className='flex items-center jutify-between w-full h-auto'>
 		<div className='flex flex-col items-start jutify-between w-full h-auto'>
 		  
 
-		    <h1 className='md:text-3xl text-2xl tracking-wide mb-3'>RARE STUDIOS NEW YORK</h1>
-		    <h1 className='md:text-sm text-xs'>&#169;2023 RARE STUDIOS NEW YORK.All Rights Reserved.</h1>
+		    <h1 className='md:text-2xl text-xl mb-3 font-semibold'>RARE STUDIOS NEW YORK</h1>
+		    <h1 className='md:text-sm text-xs tracking-wide'>&#169;2023 RARE STUDIOS NEW YORK.All Rights Reserved.</h1>
 		  
 		</div>
 		
-		<img src='/footer-icon-right.png' className='md:h-[80px] h-[50px] w-auto'/>
+		<img src='/footer-icon-right.png' className='md:h-[50px] h-[50px] w-auto'/>
 	</div>	
 </footer>
 
