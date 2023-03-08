77bb1c0f6e89ba1e3858e145687a282982eeb02a
Codehash001 <codehashblockchain@gmail.com>
2023-03-08 18:11:14 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Index() {
         <link rel="icon" href="/Logoicon.png" />
       </Head>
        <div className='flex flex-col items-center-justify-center w-screen h-screen overflow-hidden'>
-       	  <img/ src='/rarestudios-site-bg-new.png' className='w-screen h-screen fixed top-0 -z-10'>
+       	  <img/ src='/rarestudios-site-bg-new.png' className='w-screen h-screen fixed top-0 -z-10'/>
        </div> 
     </>
   )
