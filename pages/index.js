411ae3b65a8c037f70cc2855fb2ed24b1fb03d8b
Codehash001 <codehashblockchain@gmail.com>
2023-03-09 00:28:01 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,13 @@ export default function Index() {
         <meta name="Description" content="skelee-mini-web" />
         <link rel="icon" href="/Logoicon.png" />
       </Head>
-      <div className="flex flex-col w-screen h-screen overflow-hidden">
+      <div className="flex flex-col w-screen h-screen overflow-x-hidden">
         <img
           src="/rarestudiosnewyork-sitebg.png"
           className="object-cover h-screen w-screen absolute"
         />
 
-        <div className="relative flex flex-col justify-between w-screen h-screen overflow-hidden">
+        <div className="relative flex flex-col justify-between w-screen h-screen overflow-x-hidden">
           {/* header */}
 		  <Header/>
           <div className="w-full">
