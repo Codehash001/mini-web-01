0cdc125e1e6b648774cca22e138cb8da6e9566c5
Codehash001 <codehashblockchain@gmail.com>
2023-03-09 03:14:30 +0530

    css fix

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Index() {
           </a>
         </div>
 
-        <Link className="cursor-pointer mt-4" activeClass ="active" to='footer' spy={true} smooth={true} offset={-70} duration={500}>
+        <Link className="cursor-pointer mt-4" activeClass ="active" to='footer' spy={true} smooth={true} offset={-70} duration={600}>
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" className="fill-white animate-bounce"><path d="m18.707 12.707-1.414-1.414L13 15.586V6h-2v9.586l-4.293-4.293-1.414 1.414L12 19.414z"></path></svg>
         </Link>
         </div>
