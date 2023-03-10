59b9031e5b8c0b821b8d9e4ac9f9f66a8a5adddc
Codehash001 <codehashblockchain@gmail.com>
2023-03-10 10:23:28 +0530

    css fix

--- components/FAQs.js
+++ components/FAQs.js
@@ -82,7 +82,7 @@ export default function FAQs() {
              }</div>
           </div>
         </AccordionHeader>
-        <AccordionBody className='flex flex-col items-start'>
+        <AccordionBody className='flex flex-col md:items-start items-center'>
          <div className={open == 3 ? "md:flex items-start justify-start" : 'h-0 hidden'}>
 
           <div className=" flex flex-col items-start w-full h-full p-4 bg-gray-800">
@@ -92,7 +92,7 @@ export default function FAQs() {
             <h1 className="font-medium text-white text-[16px] mt-3">Entrepreneur <br/> & Blockchain OG</h1>
           </div>
           
-          <div className=" flex flex-col items-start w-full h-full p-4 bg-gray-800 md:mx-5">
+          <div className=" flex flex-col items-start w-full h-full p-4 bg-gray-800 md:mx-5 md:my-0 my-4">
             <img src="team-4.png" className="w-[150px] h-[180px]"/>
             <h1 className="font-semibold text-white text-[18px] mt-1">Ordinal.jpg</h1>
             <h1 className="font-medium text-white text-[15px]">Founder</h1>
